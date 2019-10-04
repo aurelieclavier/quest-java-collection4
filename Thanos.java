8e0c69419b4bab740f48425fa54c2dd810c8886a
aurelie <aurelie.cissokho@gmail.com>
2019-10-04 10:17:38 +0200

    Collection 04 - Maps

--- Thanos.java
+++ Thanos.java
@@ -1,30 +1,54 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Thanos {
 
     public static void main(String[] args) {
 
-        Hero hulk = new Hero("Hulk", 49);
-        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero hulk 			= new Hero("Hulk", 49);
+        Hero blackWidow 	= new Hero("Black Widow", 34);
         Hero captainAmerica = new Hero("Captain America", 100);
-        Hero thor = new Hero("Thor", 1501);
-        Hero vision = new Hero("Vision", 3);
-        Hero scarletWitch = new Hero("Scarlet Witch", 29);
-        Hero doctorStrange = new Hero("Doctor Strange", 42);
+        Hero thor 			= new Hero("Thor", 1501);
+        Hero vision 		= new Hero("Vision", 3);
+        Hero scarletWitch 	= new Hero("Scarlet Witch", 29);
+        Hero doctorStrange 	= new Hero("Doctor Strange", 42);
 ;
-        Flower lily = new Flower("Lily");
-        Flower tulip = new Flower("Tulip");
-        Flower begonia = new Flower("Begonia");
-        Flower violet = new Flower("Violet");
-        Flower rose = new Flower("Rose");
+        Flower lily 	= new Flower("Lily");
+        Flower tulip 	= new Flower("Tulip");
+        Flower begonia 	= new Flower("Begonia");
+        Flower violet 	= new Flower("Violet");
+        Flower rose 	= new Flower("Rose");
 
         // TODO 1 : Create a TreeMap where :
         // Hulk, Thor and Scarlet Witch have a Rose
         // Vision has a Tulip
         // Captain America has a Lily
         // Doctor Strange and Black Widow have a Violet
-
+        
+        Comparator<Hero> comparatorHero = new Comparator<Hero>() {  
+            @Override  
+            public int compare(Hero orig, Hero compare) {  
+                return orig.getName().compareTo(compare.getName());  
+            }  
+        };  
+        Map<Hero, Flower> party = new TreeMap<>(comparatorHero);
+        
+        party.put(hulk, rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
+        party.put(vision, tulip);
+        party.put(captainAmerica, lily);
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
+        
         // TODO 2 : Print if `begonia` is contained in the TreeMap
+        System.out.println(party.containsValue(begonia));
 
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Hero heroes : party.keySet()) {
+            Flower flower = party.get(heroes);
+            System.out.println(flower.getName());
+        }
     }
 }
